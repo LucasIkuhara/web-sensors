bed4944789f1e61db613f87a9bbb8f60e0764548
Lucas Ikuhara <lri2911@gmail.com>
2023-12-27 20:18:12 -0300

    feat: start gps extractor implementation

--- src/adapters/GpsService.ts
+++ src/adapters/GpsService.ts
@@ -1,14 +1,39 @@
 import type { IExtractor } from "../ports/IExtractor";
 
-export const dummy = 2;
+/**
+ * A GPS extractor based on the Geolocation WebAPI.
+ */
+export class GpsExtractor implements IExtractor<GeolocationPosition> {
 
-type Geolocation = {
-    longitude: number,
-    latitude: number
-}
-export class GpsExtractor implements IExtractor<Geolocation> {
+    private _useHighPrecision: boolean;
+    private _watchId: number;
+    private _buffer: GeolocationPosition;
+
+    constructor(useHighPrecision: boolean) {
+
+        this._useHighPrecision = useHighPrecision;
+        this._watchId = this.setup();
+    }
+
+    setup(): number {
     
-    getData(): Geolocation {
+        // Fails if the context isn't capable of geolocation.
+        if (~("geolocation" in navigator)) 
+            throw new Error("GPS unavailable in the context.")
+
+        // Start watching the GPS signal.
+        const watchId = navigator.geolocation.watchPosition((position) => {
+            this._buffer = position
+        });
+
+        return watchId;
+    }
+
+    getData(): GeolocationPosition {
+        return this._buffer;
+    }
+
+    async refresh(): Promise<GeolocationPosition> {
         
     }
 }
